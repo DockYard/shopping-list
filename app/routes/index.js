b14829e3722d5506a7a27565bc5b5b967b635a99
Marten Schilstra <marten.schilstra@dockyard.com>
2018-02-26 10:54:00 +0100

    4 - Make a code change

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+    alert('Hello World');
     return this.get('store').liveQuery(q => q.findRecords('shoppingList'));
   }
 });
